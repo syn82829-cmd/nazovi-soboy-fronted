1117b913f906e1086294d150edc9b97bdbed081f
delihop <syn82829@gmail.com>
2026-07-05 16:14:28 +0300

    Bind visual viewport in app shell

--- src/app/App.jsx
+++ src/app/App.jsx
@@ -2,12 +2,18 @@ import { useEffect } from 'react';
 
 import { OpeningFlow } from '../screens/OpeningFlow/OpeningFlow.jsx';
 import { openTelegramFullscreen } from './telegramViewport.js';
+import { bindVisualViewport } from './visualViewport.js';
 
 import './App.css';
 
 export function App() {
   useEffect(() => {
+    const unbindVisualViewport = bindVisualViewport();
     openTelegramFullscreen();
+
+    return () => {
+      unbindVisualViewport();
+    };
   }, []);
 
   return (
