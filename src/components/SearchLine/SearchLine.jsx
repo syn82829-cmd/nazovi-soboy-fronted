e1d8456e262b85a9d4a82ab3b0c717260d75ae05
delihop <syn82829@gmail.com>
2026-07-05 16:01:16 +0300

    Prevent input focus before confirmation

--- src/components/SearchLine/SearchLine.jsx
+++ src/components/SearchLine/SearchLine.jsx
@@ -1,15 +1,25 @@
 import './SearchLine.css';
 
-export function SearchLine({ value, onChange, onSubmit, onSearchIntent }) {
+export function SearchLine({ value, onChange, onSubmit, onSearchIntent, inputRef, isOpen }) {
+  function handlePointerDown(event) {
+    if (isOpen) {
+      return;
+    }
+
+    event.preventDefault();
+    onSearchIntent();
+  }
+
   return (
-    <form className="search-line" onSubmit={onSubmit} onClick={onSearchIntent}>
-      <span className="search-line__ghost">я...</span>
+    <form className="search-line" onSubmit={onSubmit} onPointerDown={handlePointerDown}>
+      {!value && <span className="search-line__ghost">я...</span>}
 
       <input
+        ref={inputRef}
         className="search-line__input"
         value={value}
         onChange={onChange}
-        onFocus={onSearchIntent}
+        readOnly={!isOpen}
         aria-label="Продолжить я"
         autoComplete="off"
         autoCapitalize="none"
